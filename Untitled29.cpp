eb36e18f30138295a6b6d531cf7f8bb6f8aae144
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2022-10-04 23:50:07 +0700

    Create Untitled29.cpp

---
+++ Untitled29.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	int a, b;
+	scanf("%d%d", &a, &b);
+	if ( a >= b) {
+		printf("a is greater or equal than to b");
+	
+	} else {
+		printf("a is smaller than b");
+	}
+	return 0;
+}
